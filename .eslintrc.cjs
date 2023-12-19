726666d1df270e42d976278660d262b6662b1de6
matveyb <matvey.b2022@gmail.com>
2023-12-19 12:50:17 +0100

    project init

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+  ],
+  rules: {
+    'vue/html-indent': [
+      'error',
+      2,
+    ],
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'always',
+    }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-shadow': [2, { allow: ['state', 'getters'] }],
+    'no-param-reassign': ['error', {
+      props: true, ignorePropertyModificationsFor: ['state'],
+    }],
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 1
+      },      
+      "multiline": {
+        "max": 1
+      }
+    }]
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+};
