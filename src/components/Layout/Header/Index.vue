a7eb465a707108f48ec24e2666a2f0ddb30e00a1
Kron1749 <drakgr98@gmail.com>
2024-01-11 17:41:46 +0300

    display_modal_component

--- src/components/Layout/Header/Index.vue
+++ src/components/Layout/Header/Index.vue
@@ -17,6 +17,25 @@
           text="CONNECT"
           @click="connectWallet"
         >CONNECT</ButtonComponent>
+        <ButtonComponent
+          type="button"
+          @click="openModal"
+        >Open Modal</ButtonComponent>
+        <ModalComponent v-model="showModal">
+          <template #header>
+            <h2 style="padding: 10px 0 0;">Привет</h2>
+          </template>
+
+          <p>Я - модальное окно. Закрой меня скорее.</p>
+
+          <template #footer>
+            <ButtonComponent
+              label="Закрыть"
+              style="margin: 0 auto;"
+              @click="showModal = false"
+            />
+          </template>
+        </ModalComponent>
       </div>
       <WalletComponent
         v-if="false"
@@ -29,6 +48,7 @@
 import { mapGetters } from 'vuex';
 import WalletComponent from '@/components/Layout/Header/WalletComponent.vue';
 import ButtonComponent from '@/components/Button/Index.vue';
+import ModalComponent from '@/components/Layout/Header/ModalComponent.vue';
 import Logo from '@/assets/icons/header/IconLogo.vue';
 
 export default {
@@ -36,7 +56,13 @@ export default {
   components: {
     WalletComponent,
     ButtonComponent,
+    ModalComponent,
     Logo,
+  },
+  data() {
+    return {
+      showModal: false,
+    };
   },
   computed: {
     ...mapGetters('walletAction', ['walletConnected']),
@@ -49,6 +75,11 @@ export default {
     connectWallet() {
       console.log('connectWallet');
       this.$store.dispatch('walletAction/connectWallet');
+    },
+    openModal() {
+      console.log('Open Modal clicked');
+      this.showModal = true;
+      console.log('showModal:', this.showModal);
     },
   },
 };
---
+++ src/components/Layout/Header/ModalComponent.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="modal__activator">
+    <slot
+      name="activator"
+      :on="activator"
+    />
+
+    <div
+      v-if="opened"
+      class="modal__wrap"
+      @click="opened = false"
+      @keydown.esc="closeModal"
+    >
+      <div
+        class="modal__wrap-stop"
+        @click.stop
+      >
+        <div class="modal__close">
+          <au-icon
+            icon="mdi-close"
+            :size="24"
+            color="#bbb"
+            @click.native="opened = false"
+          />
+        </div>
+
+        <div
+          v-show="$slots.header"
+          class="modal__header"
+        >
+          <slot name="header" />
+        </div>
+
+        <div class="modal__body">
+          <slot />
+        </div>
+
+        <div
+          v-show="$slots.footer"
+          class="modal__footer"
+        >
+          <slot name="footer" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'ModalComponent',
+
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+    pauseListeners: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      elActivator: null,
+    };
+  },
+
+  computed: {
+    opened: {
+      get() {
+        return this.modelValue;
+      },
+      set(val:any) {
+        this.$emit('update:modelValue', val);
+      },
+    },
+
+    activator() {
+      return {
+        click: this.show,
+      };
+    },
+  },
+
+  mounted() {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('keyup', this.onKeyUp);
+    }
+  },
+
+  beforeUnmount() {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('keyup', this.onKeyUp);
+    }
+  },
+
+  methods: {
+    show(e:any) {
+      e.stopPropagation();
+
+      this.elActivator = e.currentTarget;
+      this.opened = !this.opened;
+    },
+
+    onKeyUp(event:any) {
+      if (!event.keyCode || this.pauseListeners) return;
+
+      switch (event.keyCode) {
+        case 27: // esc
+          if (this.opened) this.$emit('on-escape');
+          break;
+        default:
+          break;
+      }
+    },
+    closeModal() {
+      this.$emit('update:modelValue', false);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+
+.modal__wrap {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 200;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.1);
+    user-select: none;
+
+    .modal__wrap-stop {
+        position: relative;
+        z-index: 250;
+        background-color: #fff;
+        box-shadow: 0 0 70px rgba(0, 0, 0, 0.1);
+        border-radius: 10px;
+        pointer-events: auto;
+    }
+
+    .modal__close {
+        position: absolute;
+        top: 12px;
+        right: 12px;
+        z-index: 16;
+        transition: all 0.25s ease;
+        cursor: pointer;
+
+        &:hover {
+            filter: grayscale(100%);
+        }
+    }
+
+    .modal__header,
+    .modal__body,
+    .modal__footer {
+        padding: 10px 16px;
+    }
+
+    .modal__header {
+        border-bottom: 1px;
+    }
+
+    .modal__body {
+        max-height: 82vh;
+        max-width: 90vw;
+        overflow: auto;
+    }
+
+    .modal__footer {
+        border-top: 1px;
+    }
+}
+</style>
