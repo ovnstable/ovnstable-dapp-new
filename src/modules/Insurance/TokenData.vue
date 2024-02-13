5a99e1c04d109ed59b295b38bd9c52a058a4c049
Kron1749 <drakgr98@gmail.com>
2024-02-13 16:07:49 +0300

    updated_styles insurance_address_arb_added

--- src/modules/Insurance/TokenData.vue
+++ src/modules/Insurance/TokenData.vue
@@ -9,7 +9,7 @@
         <p class="insurance__token-data-title insurance__token-data-title--token">INSURANCE</p>
         <div class="insurance__links-info">
           <a
-            :href="`${networkScan}address/` + generateHref(tokenData.tokenName, networkName)"
+            :href="`${networkScan}token/` + generateHref('token_insurance', networkName)"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="token-address"
@@ -20,7 +20,7 @@
             target="_blank"
             rel="noopener noreferrer"
             aria-label="token-address"
-            class="insurance__token-data-link-addr link-ovn"
+            class="insurance__token-data-ovn-info link-ovn"
           >What is ovn</a>
         </div>
       </div>
@@ -60,56 +60,58 @@
     </div>
 
   </div>
-  <div class="performance__token-data">
+  <div class="insurance__token-data">
 
-    <p class="performance__token-data__description">{{ tokenData.description }}</p>
-    <div class="performance__divider" />
-    <div class="performance__payout-data">
-      <p class="insurance__token-data-title">Last payout</p>
-      <p class="performance__token-data-num performance__token-data-num--payout-ago">{{ tokenData.lastPayoutTime }}</p>
-      <p class="performance__token-data-h">{{ tokenData.lastPayoutType }}</p>
+    <div class="insurance__divider insurance__divider--first-divider" />
+    <div class="insurance__payout-data">
+      <p class="insurance__token-data-title">Risk factor</p>
+      <div class="insurance_risk-factor">
+        <p class="insurance_risk-factor-text">High</p>
+        <BaseIcon
+          name='Insurance_RiskFactor'
+        />
+      </div>
     </div>
-    <div class="performance__divider" />
+    <div class="insurance__divider" />
+    <div class="insurance__payout-data">
+      <p class="insurance__token-data-title">Last payout</p>
+      <p class="insurance__token-data-num">{{ tokenData.lastPayoutTime }}</p>
+      <p class="insurance__token-data-h">{{ tokenData.lastPayoutType }}</p>
+    </div>
+    <div class="insurance__divider" />
+    <div class="insurance__payout-data">
+      <p class="insurance__token-data-title">Insurance vault</p>
+      <p class="insurance__token-data-num amount-of-ovn">{{ tokenData.amountOfOVN }} OVN</p>
+      <BaseIcon
+        name='Insurance_OVNVault'
+      />
+    </div>
+    <div class="insurance__divider" />
+    <div class="insurance__payout-data">
+      <p class="insurance__token-data-title">Value of vault</p>
+      <p class="insurance__token-data-num amount-of-ovn">${{ tokenData.valueOfVault }}</p>
+    </div>
+    <div class="insurance__divider" />
+    <div class="insurance__payout-data">
+      <p class="insurance__token-data-title">Insurance coverage</p>
+      <p class="insurance__token-data-num amount-of-ovn">{{ tokenData.percentOfCoverage }}%</p>
+    </div>
+    <div class="insurance__divider" />
     <div class="performance__apy-data">
-      <p class="insurance__token-data-title">Daily APY</p>
-      <div class="performance__apy-data-chain">
-        <p class="performance__token-data-num performance__token-data-num--apy-num">{{ tokenData.dailyApy }}%</p>
+      <p class="insurance__token-data-title">30-day APY</p>
+      <div class="insurance__apy-data-chain">
+        <p class="insurance__token-data-num insurance__token-data-num--apy-num">{{ tokenData.dailyApy }}%</p>
         <div
           class="performance__icon-chain"
         >
           <BaseIcon
-            :name=chainIcon
+            :name="getIconName(networkName)"
           />
         </div>
 
       </div>
     </div>
-    <div class="performance__divider" />
-    <div class="performance__tvl-data">
-      <p class="insurance__token-data-title">{{ tokenData.tokenName }} TVL</p>
-      <p class="performance__token-data-num">{{ tokenData.tvl }}     <span class="performance__token-data-col-token">
-        {{ tokenData.collateralToken === 'WETH' ? tokenData.collateralToken : '$' }}
-      </span></p>
-      <p class="performance__token-data-h">past 2 hours</p>
-    </div>
-    <div class="performance__divider performance__divider--last-divider" />
-  </div>
-
-  <div class="performance__chain-data-container">
-    <div
-      v-for="chain in availableChains"
-      :key="chain"
-      @click="saveNetworkToLocalStore(chain)"
-      @keydown.enter="saveNetworkToLocalStore(chain)"
-      class="performance__chain-data"
-      :class="{ selected: chain.toLowerCase() === networkName }"
-    >
-      <BaseIcon
-        :name="getIconName(chain)"
-        class="performance__icon-chain-bottom"
-      />
-      <p class="performance__chain-data-name">{{ chain }}</p>
-    </div>
+    <div class="insurance__divider insurance__divider--last-divider" />
   </div>
 
 </template>
@@ -133,15 +135,14 @@ export default {
         lastPayoutTime: '05:02',
         lastPayoutType: 'hours ago',
         dailyApy: '5',
+        amountOfOVN: 4376,
+        valueOfVault: 76074,
+        percentOfCoverage: 3,
       }),
     },
     chainIcon: {
       type: String,
       default: 'IconArbitrum',
-    },
-    chainName: {
-      type: String,
-      default: 'Optimism',
     },
   },
   computed: {
@@ -151,55 +152,16 @@ export default {
     networkScan() {
       return this.$store.state.network.explorerUrl;
     },
-    availableChains() {
-      const tokenKey = `${this.tokenData.tokenName?.toLowerCase()?.slice(0, -1)}Plus`;
-      if (!tokenKey) return [];
-      const availableNetworks = Object.entries(chainContractsMap)
-        .reduce((acc, [network, tokens]: any) => {
-          if ((tokenKey in tokens) && tokens[tokenKey].tokenPlus) {
-            acc.push(network.charAt(0).toUpperCase() + network.slice(1));
-          }
-          return acc;
-        }, [] as string[]);
-
-      return availableNetworks;
-    },
   },
   methods: {
-    saveNetworkToLocalStore(chain:string) {
-      this.$store.dispatch('network/changeDappNetwork', chain.toLowerCase());
-    },
     generateHref(tokenName:string, networkName:string) {
-      if (!tokenName) {
-        return '';
-      }
-
-      const tokenKey = `${tokenName.toLowerCase()?.slice(0, -1)}Plus`;
-      if (!tokenKey) return '';
       const networkContracts = (chainContractsMap as any)[networkName.toLowerCase()];
-      if (!networkContracts || !(tokenKey in networkContracts)) {
-        console.error(`Contracts not found for ${tokenKey} on ${networkName}`);
-        return '';
-      }
-
-      const { tokenPlus } = networkContracts[tokenKey];
-
-      if (!tokenPlus) {
-        console.error(`Address not found for ${tokenKey} on ${networkName}`);
-        return '';
-      }
-
-      return tokenPlus;
+      const tokenInsurance = networkContracts[tokenName];
+      return tokenInsurance;
     },
 
     getIconName(chain:string) {
-      const selectedChain = this.$store.state.network.networkName;
       const formattedChain = chain.charAt(0).toUpperCase() + chain.slice(1).toLowerCase();
-
-      if (chain.toLowerCase() !== selectedChain.toLowerCase()) {
-        return `Icon${formattedChain}Off`;
-      }
-
       return `Icon${formattedChain}On`;
     },
   },
@@ -235,7 +197,9 @@ export default {
     margin-right: 8px;
   }
 }
-
+.amount-of-ovn {
+  margin-bottom: 5px;
+}
 .insurance__redeem-button {
   margin-left: 8px;
 }
@@ -260,8 +224,19 @@ export default {
   border-radius: 30px;
   padding: 5px 14px;
 }
+.insurance_risk-factor-text {
+  color: var(--color-1);
+  font-size: 20px;
+  font-weight: 500;
+  margin-right: 10px  ;
+}
+.insurance_risk-factor {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+}
 
-.performance__token-data {
+.insurance__token-data {
   display:flex;
   flex-direction: row;
   justify-content: space-between;
@@ -301,7 +276,8 @@ export default {
     color: var(--color-4);
   }
 }
-.insurance__token-data-link-addr {
+.insurance__token-data-link-addr,
+.insurance__token-data-ovn-info {
   margin-right: 10px;
   color: var(--color-2);
   font-size: 12px;
@@ -310,7 +286,7 @@ export default {
     color: var(--color-18);
   }
 }
-.performance__token-data__description {
+.insurance__token-data__description {
   margin-left: 30px;
   color: var(--color-2);
   text-align: justify;
@@ -323,7 +299,7 @@ export default {
   }
 }
 
-.performance__divider {
+.insurance__divider {
   margin-left: 30px;
   margin-right: 30px;
   border: 1px solid var(--color-7);
@@ -332,45 +308,48 @@ export default {
   }
 }
 
-.performance__divider--last-divider {
+.insurance__divider--last-divider {
   margin-right: 0;
 }
+.insurance__divider--first-divider {
+  margin-left: 0;
+}
 
-.performance__payout-data,
+.insurance__payout-data,
 .performance__apy-data,
 .performance__tvl-data {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
-.performance__token-data-num,
-.performance__token-data-col-token {
+.insurance__token-data-num,
+.insurance__token-data-col-token {
   color: var(--color-1);
   font-size: 20px;
   font-weight: 500;
 }
 
-.performance__token-data-num {
+.insurance__token-data-num {
   margin-top: 20px;
   text-align: center;
   [data-theme="dark"] & {
     color: var(--color-4);
   }
 }
-.performance__token-data-num--apy-num {
+.insurance__token-data-num--apy-num {
   margin-top: 0px;
 }
-.performance__apy-data-chain {
+.insurance__apy-data-chain {
   margin-top: 20px;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
 }
 .performance__icon-chain {
-  margin-left: 10px;
+  margin-top: 8px
 }
 
-.performance__token-data-h {
+.insurance__token-data-h {
   color: var(--color-1);
   font-size: 15px;
   font-weight: 500;
@@ -450,13 +429,16 @@ export default {
     padding: 2px 5px;
     margin-top: 24px;
   }
+  .insurance__token-data-title {
+    text-align: center;
+  }
   .insurance__token-image {
     scale: 65%;
   }
-  .performance__token-data-col-token {
+  .insurance__token-data-col-token {
     margin-left: 2px;
   }
-  .performance__token-data__description {
+  .insurance__token-data__description {
     margin-left: 5px;
     max-width: 150px;
   }
@@ -467,31 +449,28 @@ export default {
   .insurance__token-data-title {
       font-size: 16px;
   }
-  .performance__token-data-num {
+  .insurance__token-data-num {
     font-size: 14px;
   }
-  .performance__token-data-h {
+  .insurance__token-data-h {
     font-size: 12px;
   }
-  .performance__divider {
+  .insurance__divider {
     margin-left: 5px;
     margin-right: 5px;
   }
 }
 
 @media (max-width: 768px) {
-  .performance__divider {
+  .insurance__divider {
     margin-left: 20px;
     margin-right: 20px;
   }
-  .performance__token-data-col-token {
+  .insurance__token-data-col-token {
     margin: 0;
   }
   .insurance__token-data-link-addr {
     margin-top: 40px;
-  }
-  .performance__token-data-num--payout-ago {
-    margin-right: 15px;
   }
   .performance__chain-data {
     padding: 2px 5px;
@@ -504,16 +483,16 @@ export default {
 }
 
 @media (max-width: 576px) {
-  .performance__token-data-num {
+  .insurance__token-data-num {
     margin-top: 60px;
   }
-  .performance__apy-data-chain {
+  .insurance__apy-data-chain {
     margin: 0;
   }
   .performance__icon-chain {
     margin-top: 60px;
   }
-  .performance__divider {
+  .insurance__divider {
     margin-left: 5px;
     margin-right: 5px;
   }
