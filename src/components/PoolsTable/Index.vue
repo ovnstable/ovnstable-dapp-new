50f08330c307df368c578fc9ceb56a8bf03883c8
Kron1749 <drakgr98@gmail.com>
2024-03-25 11:52:17 +0300

    updated_dark_theme

--- src/components/PoolsTable/Index.vue
+++ src/components/PoolsTable/Index.vue
@@ -452,7 +452,6 @@ export default {
   width: fit-content;
   border-radius: 30px;
   [data-theme="dark"] & {
-    background-color: var(--color-7);
     border: none;
   }
 
@@ -639,6 +638,10 @@ export default {
   height: 19px;
   padding: 0 4px;
   animation: pulse-animation-blue 3s infinite;
+  [data-theme="dark"] & {
+    color: var(--color-4);
+    background-color: var(--color-17);
+  }
 }
 .pools-table__hot {
   top: -12px;
@@ -667,6 +670,23 @@ export default {
   }
   .pools-header {
     grid-template-columns: 0.5fr 2.2fr 2fr 1fr 1fr 1fr;
+  }
+}
+@media (max-width: 1000px) {
+  .pools-table {
+    overflow: hidden;
+    overflow-x: auto
+  }
+
+  .pools-table__footer {
+    position: sticky;
+    left: 0;
+    padding-bottom: 30px;
+  }
+  .pools-header,
+  .pools-table__content {
+    min-width: 850px;
+    overflow-x: scroll;
   }
 }
 @media (max-width: 640px) {
--- src/modules/Main/components/Slider/Index.vue
+++ src/modules/Main/components/Slider/Index.vue
@@ -305,9 +305,6 @@ export default {
   flex-direction: row;
   align-items: center;
   margin: 50px 40px 50px auto;
-  [data-theme="dark"] & {
-    background-color: var(--color-17);
-  }
   @media (max-width: 1200px) {
     width: 50%;
     margin: 20px 10px 20px auto;
