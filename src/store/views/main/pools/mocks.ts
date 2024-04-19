096a0ff5fff2c35824e2b328ed2acaf2fe0e122f
Kron1749 <drakgr98@gmail.com>
2024-04-19 14:56:15 +0300

    ambient_updated_pools

--- src/store/views/main/pools/mocks.ts
+++ src/store/views/main/pools/mocks.ts
@@ -477,6 +477,8 @@ export enum POOL_TAG {
 export const NEW_POOLS: string[] = [
   '0xf2d0a6699fea86fff3eb5b64cdc53878e1d19d6f',
   '0x21f25b792d2e14378f93a4c3260a53f4a889e68d',
+  '0xaAaaaAAAFfe404EE9433EEf0094b6382D81fb958_eth',
+  '0xaAaaaAAAFfe404EE9433EEf0094b6382D81fb958_usdb',
 ];
 
 export const HOT_POOLS = [
