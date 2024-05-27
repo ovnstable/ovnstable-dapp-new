bb7941a5958dd1d5ac2e824567bacbda6978a35c
Leonid <zotovleonid@icloud.com>
2024-05-27 16:25:15 +0300

    minor fix

--- src/store/views/insurance/InsurancePayouts.ts
+++ src/store/views/insurance/InsurancePayouts.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-import InsuranceApiService from '@/services/insurance-api-service.ts';
+import InsuranceApiService from "@/services/insurance-api-service.ts";
 
 const state = {
   insurancePayouts: {},
@@ -17,22 +17,30 @@ const actions = {
       let counter = insurancePayouts.length;
 
       insurancePayouts.reverse().forEach((payout: any) => {
-        accumulator *= (1 + payout.dailyProfit);
-        payout.comp = ((accumulator * 100) / startValue) - 100;
-        payout.comp = parseFloat(payout.comp ? payout.comp : 0.00).toFixed(3);
-
+        const { dailyProfit } = payout;
+        if (payout.hash === "0xc0ba6d1d74f5fa3952b0d6521f05073f8bc4060429b89e75ed1e3be9c96a75f2") {
+          payout.comp = (-100.00).toFixed(3);
+          accumulator = 1;
+          counter--;
+          return;
+        }
+        accumulator *= 1 + dailyProfit;
+        payout.comp = (accumulator * 100) / startValue - 100;
+        payout.comp = parseFloat(payout.comp ? payout.comp : 0.0).toFixed(3);
         if (counter <= 7) {
-          accumulatorWeek *= (1 + payout.dailyProfit);
+          accumulatorWeek *= 1 + dailyProfit;
         }
 
         if (counter <= 30) {
-          accumulatorMonth *= (1 + payout.dailyProfit);
+          accumulatorMonth *= 1 + dailyProfit;
         }
         if (counter === 1) {
-          accumulatorDay *= (1 + payout.dailyProfit);
+          accumulatorDay *= 1 + dailyProfit;
         }
+
         counter--;
       });
+
       accumulatorDay = (accumulatorDay - 1) * 100;
       accumulatorWeek = (accumulatorWeek - 1) * 100;
       accumulatorMonth = (accumulatorMonth - 1) * 100;
@@ -42,9 +50,9 @@ const actions = {
         accumulatorWeek: accumulatorWeek.toFixed(2),
         accumulatorMonth: accumulatorMonth.toFixed(2),
       };
-      commit('setInsurancePayouts', payload);
+      commit("setInsurancePayouts", payload);
     } catch (error) {
-      console.error('Failed to fetch insurance payouts:', error);
+      console.error("Failed to fetch insurance payouts:", error);
     }
   },
 };
