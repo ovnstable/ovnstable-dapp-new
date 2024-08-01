ed096bafae135f77d72f6b21125cf82a86ba6b8d
digita1hobo <rtsivwrk@gmail.com>
2024-08-01 19:42:45 +0800

    posthog track multiple wallets

--- src/services/PosthogService/posthogConfig.ts
+++ src/services/PosthogService/posthogConfig.ts
@@ -3,7 +3,8 @@ import type { PostHogConfig } from 'posthog-js';
 const POSTHOG_CONFIG: Partial<PostHogConfig> = {
   api_host: 'https://eu.i.posthog.com',
   capture_pageview: true,
-  autocapture: true,
+  autocapture: false,
+  person_profiles: 'always',
 };
 
 export default POSTHOG_CONFIG;
