4186472f321c26f0e912e97d7917b20ca697ed09
matveyb <matvey.b2022@gmail.com>
2024-08-06 12:58:24 +0200

    base icon manage

--- src/modules/ModalTemplates/SuccessModal/components/withdraw.vue
+++ src/modules/ModalTemplates/SuccessModal/components/withdraw.vue
@@ -65,12 +65,14 @@
 import { mapState } from 'vuex';
 import PoolLabel from '@/modules/Main/components/ZapModal/PoolLabel.vue';
 import ButtonComponent from '@/components/Button/Index.vue';
+import BaseIcon from '@/components/Icon/BaseIcon.vue';
 import getPlatformLink from '../helpers.ts';
 
 export default {
   name: 'ZapinContent',
   components: {
     PoolLabel,
+    BaseIcon,
     ButtonComponent,
   },
   computed: {
