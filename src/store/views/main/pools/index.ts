633b2e9b63ffac5e8362c019f5ef204c51355c5f
digita1hobo <rtsivwrk@gmail.com>
2024-08-07 18:47:12 +0800

    pools parallel fetch

--- src/store/views/main/pools/index.ts
+++ src/store/views/main/pools/index.ts
@@ -15,6 +15,7 @@ import {
 import { loadTokenImage } from '@/utils/tokenLogo.ts';
 import { isArray } from 'lodash';
 import BigNumber from 'bignumber.js';
+import { PromisePool } from '@supercharge/promise-pool';
 import { poolTokensForZapMap } from '../zapin/mocks.ts';
 
 // eslint-disable-next-line no-shadow
@@ -34,10 +35,6 @@ const stateData = {
   isPoolsLoading: true,
 
   topPool: null,
-
-  // todo: remove after presale
-  aerodromePool: null,
-  velodromePool: null,
 
   typeOfPool: POOL_TYPES.ALL,
 
@@ -90,7 +87,7 @@ const actions = {
   },
 
   async loadPools({
-    commit, state, rootState,
+    commit, rootState,
   }: any) {
     commit('changeState', {
       field: 'isPoolsLoading',
@@ -103,125 +100,110 @@ const actions = {
     });
     const networkConfigList = [...rootState.network.allNetworkConfigs];
 
-    for (const networkConfig of networkConfigList) {
-      await poolApiService
-        .getAllPools(networkConfig.appApiUrl)
-        .then((data: any) => {
-          if (data) {
-            const buildPools = data.map((pool: any) => {
-              let tokenNames = pool.id.name.split('/');
-              let poolTag = null;
+    const getFormatPools = async (data: any, networkConfig: any): Promise<[]> => {
+      if (data) {
+        const poolsList = data.map((pool: any) => {
+          let tokenNames = pool.id.name.split('/');
+          let poolTag = null;
 
-              if (pool?.id?.name === 'Convex USD+FRAXBP') {
-                tokenNames = ['USD+', 'FRAX'];
-                pool.id.name = 'USD+/FRAXBP';
-              }
-
-              const token0Icon = loadTokenImage(tokenNames[0]);
-              const token1Icon = loadTokenImage(tokenNames[1]);
-              const token2Icon = tokenNames[2] ? loadTokenImage(tokenNames[2]) : null;
-              const token3Icon = tokenNames[3] ? loadTokenImage(tokenNames[3]) : null;
-
-              if (
-                pool
-                && zapPlatformSupportList?.includes(pool.platform)
-                && Object.keys(poolTokensForZapMap).some(
-                  (item) => item.toLowerCase() === pool.id.address.toLowerCase(),
-                )
-              ) {
-                pool.zappable = true;
-              }
-
-              // const moreThanOnePlatform = checkForSamePlatform(pool.id.address);
-
-              // unique cases, different platforms, one pool
-              // if (moreThanOnePlatform) pool.platform = moreThanOnePlatform;
-
-              if (pool && pool?.tvl >= 0) {
-                pool = initAggregators(pool);
-
-                const newName = pool.id.name.toUpperCase();
-
-                if (HOT_POOLS.includes(pool.id.address)) poolTag = POOL_TAG.HOT;
-                if (NEW_POOLS.includes(pool.id.address)) poolTag = POOL_TAG.NEW;
-                // if (LOW_TVL_PROMOTE.includes(pool.id.address)) poolTag = POOL_TAG.NEW;
-
-                return {
-                  id: pool.id.name + pool.tvl + pool.platform,
-                  name: newName,
-                  token0Icon,
-                  token1Icon,
-                  token2Icon,
-                  token3Icon,
-                  poolVersion: pool.pool_version,
-                  chain: networkConfig.networkId,
-                  chainName: networkConfig.networkName,
-                  address: pool.id.address,
-                  platform: isArray(pool.platform) ? pool.platform : [pool.platform],
-                  tvl: pool.tvl,
-                  apr: pool.apr,
-                  poolTag,
-                  skimEnabled: pool.skimEnabled,
-                  explorerUrl: networkConfig.explorerUrl,
-                  zappable: pool.zappable,
-                  cardOpened: false,
-                  data: pool,
-                  aggregators: pool.aggregators,
-                  promoted: pool.promoted,
-                  poolNameForAgregator: pool.poolNameForAgregator,
-                  isOpened:
-                    !(pool.aggregators && pool.aggregators.length),
-                  stableFishUrl: null,
-                };
-              }
-
-              return null;
-            });
-
-            const filteredPools = buildPools.filter((_: any) => !!_);
-            commit('changeState', {
-              field: 'allPools',
-              val: state.allPools?.concat(filteredPools),
-            });
+          if (pool?.id?.name === 'Convex USD+FRAXBP') {
+            tokenNames = ['USD+', 'FRAX'];
+            pool.id.name = 'USD+/FRAXBP';
           }
-        })
-        .catch((e) => {
-          console.log(`Error get pools data: ${e}`);
+
+          const token0Icon = loadTokenImage(tokenNames[0]);
+          const token1Icon = loadTokenImage(tokenNames[1]);
+          const token2Icon = tokenNames[2] ? loadTokenImage(tokenNames[2]) : null;
+          const token3Icon = tokenNames[3] ? loadTokenImage(tokenNames[3]) : null;
+
+          if (
+            pool
+            && zapPlatformSupportList?.includes(pool.platform)
+            && Object.keys(poolTokensForZapMap).some(
+              (item) => item.toLowerCase() === pool.id.address.toLowerCase(),
+            )
+          ) {
+            pool.zappable = true;
+          }
+
+          if (pool && pool?.tvl >= 0) {
+            pool = initAggregators(pool);
+
+            const newName = pool.id.name.toUpperCase();
+
+            if (HOT_POOLS.includes(pool.id.address)) poolTag = POOL_TAG.HOT;
+            if (NEW_POOLS.includes(pool.id.address)) poolTag = POOL_TAG.NEW;
+            // if (LOW_TVL_PROMOTE.includes(pool.id.address)) poolTag = POOL_TAG.NEW;
+
+            return {
+              id: pool.id.name + pool.tvl + pool.platform,
+              name: newName,
+              token0Icon,
+              token1Icon,
+              token2Icon,
+              token3Icon,
+              poolVersion: pool.pool_version,
+              chain: networkConfig.networkId,
+              chainName: networkConfig.networkName,
+              address: pool.id.address,
+              platform: isArray(pool.platform) ? pool.platform : [pool.platform],
+              tvl: pool.tvl,
+              apr: pool.apr,
+              poolTag,
+              skimEnabled: pool.skimEnabled,
+              explorerUrl: networkConfig.explorerUrl,
+              zappable: pool.zappable,
+              cardOpened: false,
+              data: pool,
+              aggregators: pool.aggregators,
+              promoted: pool.promoted,
+              poolNameForAgregator: pool.poolNameForAgregator,
+              isOpened:
+                !(pool.aggregators && pool.aggregators.length),
+              stableFishUrl: null,
+            };
+          }
+
+          return [];
         });
-    }
 
-    if (state.allPools && state.allPools.length) {
-      // init pool at aggregator
-      for (let i = 0; i < state.allPools.length; i++) {
-        const pool = state.allPools[i];
-        initReversePools(pool, state.allPools);
+        return poolsList;
       }
+      return [];
+    };
+
+    const { errors, results } = await PromisePool
+      .for(networkConfigList)
+      .process(async (networkConfig: any) => {
+        const networkPools = await poolApiService.getAllPools(networkConfig.appApiUrl);
+        return getFormatPools(networkPools, networkConfig);
+      });
+
+    if (errors.length > 0) {
+      console.log(`Error get pools data: ${errors}`);
+    } else {
+      const poolsList = results.flat();
 
       commit('changeState', {
-        field: 'velodromePool',
-        val: state.allPools.find(
-          (pool: any) => pool.address
-            === '0x844D7d2fCa6786Be7De6721AabdfF6957ACE73a0_Velodrome',
-        ),
+        field: 'allPools',
+        val: poolsList,
       });
+
+      // TODO: Create Map and call initReversePools once accessing by address
+      poolsList.forEach((pool: any) => initReversePools(pool, poolsList));
+
+      const sortedPools = getSortedPools(poolsList, false);
+
       commit('changeState', {
-        field: 'aerodromePool',
-        val: state.allPools.find(
-          (pool: any) => pool.address
-            === '0x61366A4e6b1DB1b85DD701f2f4BFa275EF271197_Aerodrome',
-        ),
+        field: 'sortedPoolList',
+        val: sortedPools,
+      });
+
+      commit('changeState', {
+        field: 'isPoolsLoading',
+        val: false,
       });
     }
-
-    commit('changeState', {
-      field: 'sortedPoolList',
-      val: getSortedPools(state.allPools, false),
-    });
-
-    commit('changeState', {
-      field: 'isPoolsLoading',
-      val: false,
-    });
   },
 };
 
