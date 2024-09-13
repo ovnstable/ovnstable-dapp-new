0df979b24ef31a7e0bce2c781b9515c65fb87b63
digita1hobo <rtsivwrk@gmail.com>
2024-09-13 21:16:49 +0800

    disable increase out of range

--- src/modules/ManagePosition/ZapForm/Increase.vue
+++ src/modules/ManagePosition/ZapForm/Increase.vue
@@ -505,6 +505,7 @@ export default {
       return false;
     },
     disableButtonMessage() {
+      if (!this.zapPool.position.isInRange) return 'OUT OF RANGE, INCREASE UNAVAILABLE';
       if (this.isEmptyForm) {
         if (this.isAvailableOnNetwork) {
           return 'SELECT TOKENS';
