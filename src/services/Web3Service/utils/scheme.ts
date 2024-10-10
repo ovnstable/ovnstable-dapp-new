fcc83e48715d4a1a4b6d9a8cfea5d23c8ad18d1c
matveyb <matvey.b2022@gmail.com>
2024-10-10 16:14:52 +0200

    scheme pools

--- src/services/Web3Service/utils/scheme.ts
+++ src/services/Web3Service/utils/scheme.ts
@@ -5,54 +5,33 @@ export enum DEPOSIT_TYPES {
   NONE = ''
 }
 
-// DEV
-// export const ZAPIN_SCHEME = {
-//   arbitrum: {
-//     pancake: {
-//       zapinAdd: '0x2540E54C91F24a8E89c36fCA9073447a291E1CC3',
-//       stake: DEPOSIT_TYPES.TRANSFER,
-//     },
-//   },
-//   base: {
-//     aerodrome: {
-//       zapinAdd: '0xb63F93A8020d0495fE9EDfE23Da3b7833F632c49',
-//       stake: DEPOSIT_TYPES.DEPOSIT,
-//     },
-//     pancake: {
-//       zapinAdd: '0x7E7F8D5547B93F38bd3B893C09A80BD0233265Dc',
-//       stake: DEPOSIT_TYPES.TRANSFER,
-//     },
-//     uniswap: {
-//       zapinAdd: '',
-//       stake: DEPOSIT_TYPES.NONE,
-//     },
-//   },
-// };
-
-// for zapins
+// for zapins dev/prod
 export const ZAPIN_SCHEME = {
   arbitrum: {
     pancake: {
+      zapinDev: '0x24C032f63Bd3De6AB47F4ADeE83D46A99a98a7F9',
       zapinAdd: '0x2540E54C91F24a8E89c36fCA9073447a291E1CC3',
-      // stakeAddress: '0xc6a2db661d5a5690172d8eb0a7dea2d3008665a3',
       stake: DEPOSIT_TYPES.TRANSFER,
     },
     uniswap: {
+      zapinDev: '0x767bf60d3D22B3bfad879ff4bF311Ef1594b4B0c',
       zapinAdd: '0x767bf60d3D22B3bfad879ff4bF311Ef1594b4B0c',
       stake: DEPOSIT_TYPES.NONE,
     },
   },
   base: {
     aerodrome: {
+      zapinDev: '0xb63F93A8020d0495fE9EDfE23Da3b7833F632c49',
       zapinAdd: '0xb63F93A8020d0495fE9EDfE23Da3b7833F632c49',
       stake: DEPOSIT_TYPES.DEPOSIT,
     },
     pancake: {
+      zapinDev: '0x7E7F8D5547B93F38bd3B893C09A80BD0233265Dc',
       zapinAdd: '0x7E7F8D5547B93F38bd3B893C09A80BD0233265Dc',
-      // stakeAddress: '0xc6a2db661d5a5690172d8eb0a7dea2d3008665a3',
       stake: DEPOSIT_TYPES.TRANSFER,
     },
     uniswap: {
+      zapinDev: '0x68A213C21C9DBB6A38646B860ef10a9a95B85Da4',
       zapinAdd: '0x68A213C21C9DBB6A38646B860ef10a9a95B85Da4',
       stake: DEPOSIT_TYPES.NONE,
     },
